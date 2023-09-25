829e516d66513146fcbe82151108d4b7705bda13
unknown <stephenoluwafemi52@gmail.com>
2023-09-25 10:26:11 +0100

    new commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import TransactionTable from "./components/TransactionTable";
+import TransactionForm from "./components/TransactionForm";
+import SearchBar from "./components/SearchBar";
+
+function App() {
+  const [transactions, setTransactions] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    // Fetch data from the provided API
+    fetch("http://localhost:3000/transactions")
+      .then((response) => response.json())
+      .then((data) => setTransactions(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  // useEffect(() => {
+  //   // Fetch data from the JSON API
+  //   fetch("http://localhost:3000/transactions")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setTransactions(data);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error fetching data:", error);
+  //     });
+  // }, []);
+
+  const filteredTransactions = transactions.filter((transaction) =>
+    transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div className="App">
+      <h1>Bank Transactions</h1>
+      <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <TransactionTable transactions={filteredTransactions} />
+      <TransactionForm transactions={transactions} />
+    </div>
+  );
+}
+
+export default App;
